de769d093c5a3d70b2ea7361f6db9e364bb449bd
Thiago_Cruz <101190863+mistickesterio@users.noreply.github.com>
2023-02-23 22:11:57 -0300

    seccond commit

--- Atv_DS_3/ATV_2.Designer.cs
+++ Atv_DS_3/ATV_2.Designer.cs
@@ -1,4 +1,4 @@
-﻿
+
 namespace Atv_DS_3
 {
     partial class frmATV_2
@@ -30,19 +30,19 @@ namespace Atv_DS_3
         private void InitializeComponent()
         {
             this.atv_1 = new System.Windows.Forms.Button();
-            this.txtMês_2 = new System.Windows.Forms.TextBox();
+            this.txtMes_2 = new System.Windows.Forms.TextBox();
             this.lblIdade = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.lblNome = new System.Windows.Forms.Label();
-            this.txtMês_1 = new System.Windows.Forms.TextBox();
-            this.txtMês_5 = new System.Windows.Forms.TextBox();
+            this.txtMes_1 = new System.Windows.Forms.TextBox();
+            this.txtMes_5 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtMês_4 = new System.Windows.Forms.TextBox();
-            this.txtMês_6 = new System.Windows.Forms.TextBox();
+            this.txtMes_4 = new System.Windows.Forms.TextBox();
+            this.txtMes_6 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.txtMês_3 = new System.Windows.Forms.TextBox();
+            this.txtMes_3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // atv_1
@@ -56,15 +56,15 @@ namespace Atv_DS_3
             this.atv_1.UseVisualStyleBackColor = true;
             this.atv_1.Click += new System.EventHandler(this.atv_1_Click);
             // 
-            // txtMês_2
+            // txtMes_2
             // 
-            this.txtMês_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_2.Location = new System.Drawing.Point(123, 91);
-            this.txtMês_2.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_2.Name = "txtMês_2";
-            this.txtMês_2.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_2.TabIndex = 10;
-            this.txtMês_2.Text = "0";
+            this.txtMes_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_2.Location = new System.Drawing.Point(123, 91);
+            this.txtMes_2.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_2.Name = "txtMes_2";
+            this.txtMes_2.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_2.TabIndex = 10;
+            this.txtMes_2.Text = "0";
             // 
             // lblIdade
             // 
@@ -100,25 +100,25 @@ namespace Atv_DS_3
             this.lblNome.TabIndex = 7;
             this.lblNome.Text = "Mês 1";
             // 
-            // txtMês_1
+            // txtMes_1
             // 
-            this.txtMês_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_1.Location = new System.Drawing.Point(123, 50);
-            this.txtMês_1.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_1.Name = "txtMês_1";
-            this.txtMês_1.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_1.TabIndex = 6;
-            this.txtMês_1.Text = "0";
+            this.txtMes_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_1.Location = new System.Drawing.Point(123, 50);
+            this.txtMes_1.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_1.Name = "txtMes_1";
+            this.txtMes_1.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_1.TabIndex = 6;
+            this.txtMes_1.Text = "0";
             // 
-            // txtMês_5
+            // txtMes_5
             // 
-            this.txtMês_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_5.Location = new System.Drawing.Point(333, 91);
-            this.txtMês_5.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_5.Name = "txtMês_5";
-            this.txtMês_5.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_5.TabIndex = 15;
-            this.txtMês_5.Text = "0";
+            this.txtMes_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_5.Location = new System.Drawing.Point(333, 91);
+            this.txtMes_5.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_5.Name = "txtMes_5";
+            this.txtMes_5.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_5.TabIndex = 15;
+            this.txtMes_5.Text = "0";
             // 
             // label1
             // 
@@ -142,25 +142,25 @@ namespace Atv_DS_3
             this.label2.TabIndex = 13;
             this.label2.Text = "Mês 4";
             // 
-            // txtMês_4
+            // txtMes_4
             // 
-            this.txtMês_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_4.Location = new System.Drawing.Point(333, 50);
-            this.txtMês_4.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_4.Name = "txtMês_4";
-            this.txtMês_4.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_4.TabIndex = 12;
-            this.txtMês_4.Text = "0";
+            this.txtMes_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_4.Location = new System.Drawing.Point(333, 50);
+            this.txtMes_4.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_4.Name = "txtMes_4";
+            this.txtMes_4.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_4.TabIndex = 12;
+            this.txtMes_4.Text = "0";
             // 
-            // txtMês_6
+            // txtMes_6
             // 
-            this.txtMês_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_6.Location = new System.Drawing.Point(333, 132);
-            this.txtMês_6.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_6.Name = "txtMês_6";
-            this.txtMês_6.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_6.TabIndex = 19;
-            this.txtMês_6.Text = "0";
+            this.txtMes_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_6.Location = new System.Drawing.Point(333, 132);
+            this.txtMes_6.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_6.Name = "txtMes_6";
+            this.txtMes_6.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_6.TabIndex = 19;
+            this.txtMes_6.Text = "0";
             // 
             // label3
             // 
@@ -184,15 +184,15 @@ namespace Atv_DS_3
             this.label4.TabIndex = 17;
             this.label4.Text = "Mês 3";
             // 
-            // txtMês_3
+            // txtMes_3
             // 
-            this.txtMês_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMês_3.Location = new System.Drawing.Point(123, 132);
-            this.txtMês_3.Margin = new System.Windows.Forms.Padding(2);
-            this.txtMês_3.Name = "txtMês_3";
-            this.txtMês_3.Size = new System.Drawing.Size(100, 26);
-            this.txtMês_3.TabIndex = 16;
-            this.txtMês_3.Text = "0";
+            this.txtMes_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMes_3.Location = new System.Drawing.Point(123, 132);
+            this.txtMes_3.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMes_3.Name = "txtMes_3";
+            this.txtMes_3.Size = new System.Drawing.Size(100, 26);
+            this.txtMes_3.TabIndex = 16;
+            this.txtMes_3.Text = "0";
             // 
             // frmATV_2
             // 
@@ -200,20 +200,20 @@ namespace Atv_DS_3
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(484, 261);
-            this.Controls.Add(this.txtMês_6);
+            this.Controls.Add(this.txtMes_6);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txtMês_3);
-            this.Controls.Add(this.txtMês_5);
+            this.Controls.Add(this.txtMes_3);
+            this.Controls.Add(this.txtMes_5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtMês_4);
+            this.Controls.Add(this.txtMes_4);
             this.Controls.Add(this.atv_1);
-            this.Controls.Add(this.txtMês_2);
+            this.Controls.Add(this.txtMes_2);
             this.Controls.Add(this.lblIdade);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.lblNome);
-            this.Controls.Add(this.txtMês_1);
+            this.Controls.Add(this.txtMes_1);
             this.Name = "frmATV_2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ATV_2";
@@ -225,18 +225,18 @@ namespace Atv_DS_3
         #endregion
 
         private System.Windows.Forms.Button atv_1;
-        private System.Windows.Forms.TextBox txtMês_2;
+        private System.Windows.Forms.TextBox txtMes_2;
         private System.Windows.Forms.Label lblIdade;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label lblNome;
-        private System.Windows.Forms.TextBox txtMês_1;
-        private System.Windows.Forms.TextBox txtMês_5;
+        private System.Windows.Forms.TextBox txtMes_1;
+        private System.Windows.Forms.TextBox txtMes_5;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtMês_4;
-        private System.Windows.Forms.TextBox txtMês_6;
+        private System.Windows.Forms.TextBox txtMes_4;
+        private System.Windows.Forms.TextBox txtMes_6;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtMês_3;
+        private System.Windows.Forms.TextBox txtMes_3;
     }
 }
